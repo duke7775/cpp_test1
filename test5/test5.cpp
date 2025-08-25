fcba09f502dc2451abea4ff8b8dc1214ceacbd57
Your Name <dukelu999@gmail.com>
2025-08-25 20:38:51 +0700

    test5

--- test5/test5.cpp
+++ test5/test5.cpp
@@ -1,4 +1,4 @@
-﻿// test5.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// test5.cpp : 
 //
 
 #include <stdio.h>
@@ -8,9 +8,9 @@
 //声明全局变量
 int g_val = 10;
 
-
-bool Enroll(int age) {
-	int g_val = 20;
+//判断年龄是否符合入园要求
+bool CheckEnrollment(int age) {
+	g_val = 20;
 	printf("Enroll函数中g_val的值为: %d\n", g_val);
 
 	if (3 < age && age < 7) {
@@ -21,8 +21,9 @@ bool Enroll(int age) {
 	return false;
 }
 
+//在数组中查找目标元素
 int find_target(int arr[], int size, int target) {
-	int g_val = 30;
+	g_val = 30;
 	printf("find_target函数中的g_val的值为: %d\n", g_val);
 
 	int i = 0;
@@ -43,7 +44,7 @@ int main()
 {
 	printf("全局变量g_val的值为: %d\n", g_val);
 	int age = 5;
-	Enroll(age);
+	CheckEnrollment(age);
 	
 	int arr[] = { 1,2,3,4,5};
 	int target = 11;
@@ -53,14 +54,3 @@ int main()
 	getchar();
 	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
