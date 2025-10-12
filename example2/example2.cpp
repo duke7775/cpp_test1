ff5561d94567018e4a648e98d468780f5c6f2e3e
Your Name <dukelu999@gmail.com>
2025-10-12 21:32:49 +0700

    example2

---
+++ example2/example2.cpp
@@ -0,0 +1,33 @@
+﻿//输入某年某月某日，判断这一天是这一年的第几天？
+#include <stdio.h>
+
+
+int isleapyear(int year) {
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+		return 1;
+	}
+	return 0;
+}
+
+
+int main() {
+	int year, month, day;
+	int daysinmonth[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int totaldays = 0;
+	printf("输入年,月,日");
+	scanf_s("%d%d%d", &year, &month, &day);
+
+	if (isleapyear(year)) {
+        daysinmonth[1] = 29;
+
+	}
+	for (int i = 0; i < month - 1; i++) {
+		totaldays += daysinmonth[i];
+
+	}
+	totaldays += day;
+	printf("%d年%d月%d日是今年的第%d 天\n", year, month, day, totaldays);
+	getchar();
+	return 0;
+
+}
