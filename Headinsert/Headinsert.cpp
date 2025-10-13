14ada60729e85ddd81ac75cc41136ae5a3980764
Your Name <dukelu999@gmail.com>
2025-10-13 19:28:18 +0700

    Headinsert

--- Headinsert/Headinsert.cpp
+++ Headinsert/Headinsert.cpp
@@ -2,41 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {  //定义节点结构体
- 	int data; //数据域
-	struct Node* next;//指针域
-} Node, LinkList;
+typedef struct node {
+	int data;
+	struct node* next;
+}Node;
 
 
-LinkList* insertListHead(LinkList* list, int data) {
-	LinkList* newNode = (LinkList*)malloc(sizeof(LinkList));//创建新节点
-	newNode->data = data; // 赋值
-	newNode->next = list->next; //新节点指向原来的第一个节点
-	list->next = newNode;  //头节点指向新节点
-	return list;
+void headinsert(Node *list, int data) {
+	Node* newnode = (Node*)malloc(sizeof(Node));
+	newnode->data = data;
+	newnode->next = list->next;
+	list->next = newnode;
 }
 
-void printList(LinkList* list) { //打印链表
-	LinkList* p = list->next;//指向第一个节点
-	while (p) { //遍历链表,当指针为空时结束
-		printf("%d ", p->data);
-		p = p->next;
+void printlist(Node* list) {
+	for (Node* p = list->next; p; p = p->next) {
+
+
+		printf("%d", p->data);
 	}
-	printf("\n");
+	
 }
 
-int main()
-{
-	LinkList* list = (LinkList*)malloc(sizeof(LinkList));//创建头节点
-	list->next = NULL;//初始化头节点
-	//头插入
-	list = insertListHead(list, 3);
-	list = insertListHead(list, 5);
-	list = insertListHead(list, 7);
-
-	printList(list);
+int main() {
+	Node list = { 0,NULL };
+	headinsert(&list, 3);
+	headinsert(&list, 5);
+	headinsert(&list, 6);
+	printlist(&list);
 	getchar();
 	return 0;
 
-	
 }
