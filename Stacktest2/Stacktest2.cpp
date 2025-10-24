02dd66a876f1e4a640f57f15133f39b21f447516
Your Name <dukelu999@gmail.com>
2025-10-24 20:54:29 +0700

    Stacktest2

--- Stacktest2/Stacktest2.cpp
+++ Stacktest2/Stacktest2.cpp
@@ -1,8 +1,12 @@
-﻿#include<stdio.h>
-#include<stdlib.h>
+﻿#include<stdlib.h>
+#include<stdio.h>
 
+struct Node {
+	int data;
+	struct Node* next;
+};
 struct Stack {
-	int* array;
+	Node* array;
 	int top;
 	int size;
 };
@@ -10,8 +14,8 @@ struct Stack {
 struct Stack* create_stack() {
 	Stack* stack = (Stack*)malloc(sizeof(Stack));
 	stack->top = -1;
-	stack->size = 10;
-	stack->array = (int*)malloc(stack->size * sizeof(int));
+	stack->size = 5;
+	stack->array = (Node*)malloc(stack->size * sizeof(Node));
 	return stack;
 
 }
@@ -19,11 +23,15 @@ struct Stack* create_stack() {
 void push(Stack* stack, int value) {
 	if (stack->top == stack->size - 1) {//扩展容量
 		stack->size *= 2;
-		stack->array = (int*)malloc(stack->size * sizeof(int));
+		Node* new_array = (Node*)malloc(stack->size * sizeof(struct Node));
+		for (int i = 0; i <= stack->top; i++) {//复制数据
+			new_array[i] = stack->array[i];
+		}
+		free(stack->array);
+		stack->array = new_array;
 	}
 	stack->top++;
-	stack->array[stack->top] = value;
-
+	stack->array[stack->top].data = value;
 }
 
 void pop(Stack* stack) {
@@ -38,18 +46,26 @@ void print_stack(Stack* stack) {
 		printf("栈为空");
 	}
 	for (int i = stack->top; i >= 0; i--) {
-		printf("%d ", stack->array[i]);
+		printf("%d ", stack->array[i].data);
 	}
 	printf("size: %d\n", stack->top+1);
 }
 
 int main() {
 	Stack* new_stack = create_stack();
-	push(new_stack, 10);
+	push(new_stack, 1);
 	print_stack(new_stack);
-	push(new_stack, 20);
+	push(new_stack, 2);
 	print_stack(new_stack);
-	push(new_stack, 30);
+	push(new_stack, 3);
+	print_stack(new_stack);
+	push(new_stack, 4);
+	print_stack(new_stack);
+	push(new_stack, 5);
+	print_stack(new_stack);
+	push(new_stack, 6);
+	print_stack(new_stack);
+	push(new_stack, 7);
 	print_stack(new_stack);
 	pop(new_stack);
 	print_stack(new_stack);
